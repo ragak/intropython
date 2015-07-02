ae7c021d8b75c91bc8ce811fe176cfd68cd01761
Raga Krishnakumar <raga.krishnakumar@gmail.com>
2015-07-02 08:22:42 -0700

    july2-2

--- hangman_staging.py
+++ hangman_staging.py
@@ -17,3 +17,24 @@ def isWordGuessed(secretWord, lettersGuessed):
     else:
         return False
             
+def getGuessedWord(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters and underscores that represents
+      what letters in secretWord have been guessed so far.
+    '''
+    a=''
+    if len(secretWord)==1:
+        if secretWord[0] in lettersGuessed:
+            return a+secretWord[0]
+        else:
+            return a + '_ '
+    elif len(secretWord)==0:
+        return "no secret word"
+    else:
+        if secretWord[0] in lettersGuessed:
+            a += secretWord[0] + ' '
+        else:
+            a += '_ '
+        isWordGuessed(secretWord[1:],lettersGuessed)
